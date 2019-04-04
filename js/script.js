2e8e9c65c7414d5faec929bf79455213dcce1370
Andrii <mail2andyk@gmail.com>
2019-04-04 07:55:19 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -217,4 +217,20 @@ $(function() {
         var b = $(e.target).addClass( 'down' )
         setTimeout( function() { b.removeClass( 'down' ) }, 80 )
       });
+	
+	console.log(JSON.stringify({
+  isAndroid: /Android/.test(navigator.userAgent),
+  isCordova: !!window.cordova,
+  isEdge: /Edge/.test(navigator.userAgent),
+  isFirefox: /Firefox/.test(navigator.userAgent),
+  isChrome: /Google Inc/.test(navigator.vendor),
+  isChromeIOS: /CriOS/.test(navigator.userAgent),
+  isChromiumBased: !!window.chrome && !/Edge/.test(navigator.userAgent),
+  isIE: /Trident/.test(navigator.userAgent),
+  isIOS: /(iPhone|iPad|iPod)/.test(navigator.platform),
+  isOpera: /OPR/.test(navigator.userAgent),
+  isSafari: /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent),
+  isTouchScreen: ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch,
+  isWebComponentsSupported: 'registerElement' in document && 'import' in document.createElement('link') && 'content' in document.createElement('template')
+}, null, '  '));
 });
